811712b363bb87ee48288f66a6ea9bff9fb5f3f4
Shrimp <lcamara0916@gmail.com>
2022-06-08 12:56:25 +0000

    Added some navigation to the buttons on the main menu

--- scripts/eventListeners/home.js
+++ scripts/eventListeners/home.js
@@ -1,8 +1,26 @@
 export default class Home {
     constructor() {
+
+        document.getElementById('headers').addEventListener('click', () => {
+            window.location.hash = '#headers'
+        })
+
         document.getElementById('buttons').addEventListener('click', () => {
             window.location.hash = '#buttons';
         })
+
+        document.getElementById('switches').addEventListener('click', () => {
+            window.location.hash = '#switches';
+        })
+
+        document.getElementById('borders').addEventListener('click', () => {
+            window.location.hash = '#borders';
+        })
+
+        document.getElementById('imges').addEventListener('click', () => {
+            window.location.hash = '#images';
+        })
+
     }
 
 }

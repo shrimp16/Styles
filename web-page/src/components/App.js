da5192e0657ed01d4d2664526a809b0753aa18ff
Shrimp <lcamara0916@gmail.com>
2022-07-18 22:23:49 +0000

    Implemented the router for the pages

--- web-page/src/components/App.js
+++ web-page/src/components/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Borders } from './Pages/Borders';
+import { Buttons } from './Pages/Buttons';
+import { Headers } from './Pages/Headers';
+import { Home } from './Pages/Home';
+import { Icons } from './Pages/Icons';
+import { Images } from './Pages/Images';
+import { Switches } from './Pages/Switches';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/borders' element={<Borders />}></Route>
+        <Route path='/buttons' element={<Buttons />}></Route>
+        <Route path='/headers' element={<Headers />}></Route>
+        <Route path='/icons' element={<Icons />}></Route>
+        <Route path='/images' element={<Images />}></Route>
+        <Route path='/switches' element={<Switches />}></Route>
+      </Routes>
+    </Router>
   );
 }
 

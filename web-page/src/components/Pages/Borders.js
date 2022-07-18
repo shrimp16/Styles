cb3f42ca9917f0f728aaaf69b92d9e819de8df59
Shrimp <lcamara0916@gmail.com>
2022-07-18 22:31:58 +0000

    Create new react app cuz the other one was currupted

--- web-page/src/components/Pages/Borders.js
+++ web-page/src/components/Pages/Borders.js
@@ -1,5 +1,9 @@
+import { SimpleButton } from '../../styles/Buttons.styled';
+
 export default function Borders(){
     return (
-        <></>
+        <>
+            <SimpleButton color="(255, 0, 0"></SimpleButton>
+        </>
     )
 }

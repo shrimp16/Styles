4dc5743cc042b8596d04c431b98ba4fe24e79a63
Shrimp <lcamara0916@gmail.com>
2022-07-18 22:40:27 +0000

    Deleted old react project

--- web-page/src/components/Pages/Borders.js
+++
@@ -1,9 +0,0 @@
-import { SimpleButton } from '../../styles/Buttons.styled';
-
-export default function Borders(){
-    return (
-        <>
-            <SimpleButton color="(255, 0, 0"></SimpleButton>
-        </>
-    )
-}
--- web-page/src/components/Pages/Headers.js
+++
@@ -1,5 +0,0 @@
-export default function Headers(){
-    return (
-        <></>
-    )
-}

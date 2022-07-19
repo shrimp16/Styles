3d7ebab280484c2c9f985a4ac85a94ccd17859ed
Shrimp <lcamara0916@gmail.com>
2022-07-19 21:00:39 +0000

    When clicking on the basic button it copies the styles to the clipboard, still needs some changes

--- web-app/src/Components/Buttons.js
+++ web-app/src/Components/Buttons.js
@@ -1,9 +1,17 @@
 import { SimpleButton } from '../styles/Buttons.styled';
 
 export function Buttons() {
+
+    function test(){
+        console.log(SimpleButton.componentStyle.rules);
+        console.table(SimpleButton.componentStyle.rules);
+        
+        navigator.clipboard.writeText(SimpleButton.componentStyle.rules);
+    }
+
     return (
         <>
-            <SimpleButton color="0, 0, 0">Basic Button</SimpleButton>
+            <SimpleButton onClick={test} color="0, 0, 0">Basic Button</SimpleButton>
             <SimpleButton color="255, 0, 0">Red Basic Button</SimpleButton>
         </>
     )

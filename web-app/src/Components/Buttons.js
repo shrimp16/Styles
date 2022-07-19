0674708242b8e405f59557e9516a7a5d682aa0ef
Shrimp <lcamara0916@gmail.com>
2022-07-19 20:52:46 +0000

    Implmeneted the default basic button

--- web-app/src/Components/Buttons.js
+++ web-app/src/Components/Buttons.js
@@ -3,6 +3,7 @@ import { SimpleButton } from '../styles/Buttons.styled';
 export function Buttons() {
     return (
         <>
+            <SimpleButton color="0, 0, 0">Basic Button</SimpleButton>
             <SimpleButton color="255, 0, 0">Red Basic Button</SimpleButton>
         </>
     )
--- web-app/src/styles/Buttons.styled.js
+++ web-app/src/styles/Buttons.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const SimpleButton = styled.button`
     height: 3rem;
+    width: 10rem;
     border: 3px ${props => `rgba(${props.color}, 1)`};
     border-radius: 5px;
     background-color: ${props => `rgba(${props.color}, 0.3)`};

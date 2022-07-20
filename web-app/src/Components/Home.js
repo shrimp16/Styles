ceda3b0ea07ba2e0e8955a288b2a1ad9472ea9cf
unknown <lcamara0916@gmail.com>
2022-07-20 18:03:56 +0100

    Added page navigation from the home page

--- web-app/src/Components/Home.js
+++ web-app/src/Components/Home.js
@@ -3,16 +3,21 @@ import { SimpleButton } from "../styles/Buttons.styled"
 import { HomeHeader } from "../styles/Headers.styled"
 
 export function Home(){
+
+    function goTo(page){
+        window.location.pathname = `/${page}`;
+    }
+
     return (
         <>
             <HomeHeader>Shrimp Styles</HomeHeader>
             <HomeButtonsContainer>
-                <SimpleButton color="0, 0, 0">Borders</SimpleButton>
-                <SimpleButton color="0, 0, 0">Buttons</SimpleButton>
-                <SimpleButton color="0, 0, 0">Headers</SimpleButton>
-                <SimpleButton color="0, 0, 0">Icons</SimpleButton>
-                <SimpleButton color="0, 0, 0">Images</SimpleButton>
-                <SimpleButton color="0, 0, 0">Switches</SimpleButton>
+                <SimpleButton onClick={() => goTo("borders")} color="0, 0, 0">Borders</SimpleButton>
+                <SimpleButton onClick={() => goTo("buttons")} color="0, 0, 0">Buttons</SimpleButton>
+                <SimpleButton onClick={() => goTo("headers")} color="0, 0, 0">Headers</SimpleButton>
+                <SimpleButton onClick={() => goTo("icons")} color="0, 0, 0">Icons</SimpleButton>
+                <SimpleButton onClick={() => goTo("images")} color="0, 0, 0">Images</SimpleButton>
+                <SimpleButton onClick={() => goTo("switches")} color="0, 0, 0">Switches</SimpleButton>
             </HomeButtonsContainer>
         </>
     )

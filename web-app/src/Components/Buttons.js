9b2e8bfc556380c11a35fdc102f7ff7a9ffad6bf
Shrimp <lcamara0916@gmail.com>
2022-07-21 21:40:33 +0000

    Added more colors to the buttons

--- web-app/src/Components/Buttons.js
+++ web-app/src/Components/Buttons.js
@@ -11,8 +11,12 @@ export function Buttons() {
 
     return (
         <>
-            <SimpleButton onClick={test} color="0, 0, 0">Basic Button</SimpleButton>
-            <SimpleButton color="255, 0, 0">Red Basic Button</SimpleButton>
+            <SimpleButton onClick={test} color="0, 0, 0">Simple Button</SimpleButton>
+            <SimpleButton color="255, 0, 0">Red Simple Button</SimpleButton>
+            <SimpleButton color="0, 255, 0">Green Simple Button</SimpleButton>
+            <SimpleButton color="0, 0, 255">Blue Simple Button</SimpleButton>
+            <SimpleButton color="255, 145, 0">Orange Simple Button</SimpleButton>
+            <SimpleButton color="138, 43, 226">Purple Simple Button</SimpleButton>
         </>
     )
 }

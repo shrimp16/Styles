fedb10210ec6c018d00cfe36dddf530b652d1d99
Shrimp <lcamara0916@gmail.com>
2022-07-21 21:43:38 +0000

    Implemented a container for the buttons rows

--- web-app/src/Components/Buttons.js
+++ web-app/src/Components/Buttons.js
@@ -1,22 +1,25 @@
 import { SimpleButton } from '../styles/Buttons.styled';
+import { ButtonsContainer } from '../styles/Containers.styled';
 
 export function Buttons() {
 
-    function test(){
+    function test() {
         console.log(SimpleButton.componentStyle.rules);
         console.table(SimpleButton.componentStyle.rules);
-        
+
         navigator.clipboard.writeText(SimpleButton.componentStyle.rules);
     }
 
     return (
         <>
-            <SimpleButton onClick={test} color="0, 0, 0">Simple Button</SimpleButton>
-            <SimpleButton color="255, 0, 0">Red Simple Button</SimpleButton>
-            <SimpleButton color="0, 255, 0">Green Simple Button</SimpleButton>
-            <SimpleButton color="0, 0, 255">Blue Simple Button</SimpleButton>
-            <SimpleButton color="255, 145, 0">Orange Simple Button</SimpleButton>
-            <SimpleButton color="138, 43, 226">Purple Simple Button</SimpleButton>
+            <ButtonsContainer>
+                <SimpleButton onClick={test} color="0, 0, 0">Simple Button</SimpleButton>
+                <SimpleButton color="255, 0, 0">Red Simple Button</SimpleButton>
+                <SimpleButton color="0, 255, 0">Green Simple Button</SimpleButton>
+                <SimpleButton color="0, 0, 255">Blue Simple Button</SimpleButton>
+                <SimpleButton color="255, 145, 0">Orange Simple Button</SimpleButton>
+                <SimpleButton color="138, 43, 226">Purple Simple Button</SimpleButton>
+            </ButtonsContainer>
         </>
     )
 }
---
+++ web-app/src/styles/Containers.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: fit-content;
+    margin: auto;
+
+    * {
+        margin: 5px;
+    }
+`

2805c8f2297cf1a76454c7707aae8862294d6c95
Shrimp <lcamara0916@gmail.com>
2022-07-23 13:49:46 +0000

    Added more colors to the squared switches

--- web-app/src/Components/Switches.js
+++ web-app/src/Components/Switches.js
@@ -5,7 +5,27 @@ import { SquaredSwitch } from '../styles/Switches.styled';
 export function Switches() {
     return (
         <>
-            <SquaredSwitch color="00, 0, 0">
+            <SquaredSwitch color="0, 0, 0">
+                <input type="checkbox"></input>
+                <span></span>
+            </SquaredSwitch>
+            <SquaredSwitch color="255, 0, 0">
+                <input type="checkbox"></input>
+                <span></span>
+            </SquaredSwitch>
+            <SquaredSwitch color="0, 255, 0">
+                <input type="checkbox"></input>
+                <span></span>
+            </SquaredSwitch>
+            <SquaredSwitch color="0, 0, 255">
+                <input type="checkbox"></input>
+                <span></span>
+            </SquaredSwitch>
+            <SquaredSwitch color="255, 145, 0">
+                <input type="checkbox"></input>
+                <span></span>
+            </SquaredSwitch>
+            <SquaredSwitch color="138, 43, 226">
                 <input type="checkbox"></input>
                 <span></span>
             </SquaredSwitch>
